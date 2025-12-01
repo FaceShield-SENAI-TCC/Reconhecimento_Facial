ba9030f22c3041880cc006b26b53a36d79c961df
Alexandre Santos <alexandre.santana2201@gmail.com>
2025-12-01 13:41:16 -0300

    Ajuste de Cors para nuvem

--- FaceId/qrCode/app.py
+++ FaceId/qrCode/app.py
@@ -10,7 +10,7 @@ CORS(app, origins=[
     'http://127.0.0.1:5500',
     'http://localhost:5500',
     'http://localhost:8080',
-    'https://faceshield.onrender.com',  # Substitua pelo URL real do seu frontend
+    'https://faceshield.onrender.com',  # URL frontend
     'https://*.onrender.com'  # Permite qualquer subdomínio do Render
 ])
 
